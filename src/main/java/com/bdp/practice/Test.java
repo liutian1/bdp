3b91a2c5aea0755c95c4fe09da37f36fb5e9ecac
txliu <1176199158@qq.com>
2021-11-14 19:41:55 +0800

    feat:init project

---
+++ src/main/java/com/bdp/practice/Test.java
@@ -0,0 +1,12 @@
+package com.bdp.practice;
+
+/**
+ * @ClassName : Test
+ * @Author : txliu
+ * @Date: 2021-11-14 17:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
