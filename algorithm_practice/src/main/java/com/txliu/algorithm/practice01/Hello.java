e2682b0d1b40f76b8ddde95ba7d3c6afbd95a2ca
txliu <1176199158@qq.com>
2022-04-15 20:13:35 +0800

    feat:家里环境配置好

---
+++ algorithm_practice/src/main/java/com/txliu/algorithm/practice01/Hello.java
@@ -0,0 +1,12 @@
+package com.txliu.algorithm.practice01;
+
+/**
+ * @ClassName : Hello
+ * @Author : txliu
+ * @Date: 2022-04-15 20:12
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
