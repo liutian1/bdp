c34c5d41c9bad00844ced96c0d9760fce1c50513
txliu <1176199158@qq.com>
2022-04-15 20:34:42 +0800

    feat:家里环境配置好

---
+++ demo/src/main/java/com/txliu/demo/practice01/Hello.java
@@ -0,0 +1,12 @@
+package com.txliu.demo.practice01;
+
+/**
+ * @ClassName : Hello
+ * @Author : txliu
+ * @Date: 2022-04-15 20:31
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
