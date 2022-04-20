484c5af76da1b8bac3dfa1029094a8a83847527e
txliu <1176199158@qq.com>
2022-04-20 21:19:20 +0800

    feat:lombok

---
+++ demo/src/main/java/com/txliu/demo/practice996/Person.java
@@ -0,0 +1,14 @@
+package com.txliu.demo.practice996;
+
+import lombok.Data;
+
+/**
+ * @ClassName : Person
+ * @Author : txliu
+ * @Date: 2022-04-20 21:15
+ */
+@Data
+public class Person {
+    private String name;
+    private Integer age;
+}
---
+++ demo/src/main/java/com/txliu/demo/practice996/PersonTest.java
@@ -0,0 +1,12 @@
+package com.txliu.demo.practice996;
+
+/**
+ * @ClassName : PersonTest
+ * @Author : txliu
+ * @Date: 2022-04-20 21:16
+ */
+public class PersonTest {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
