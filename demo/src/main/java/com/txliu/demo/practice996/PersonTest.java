8d5f5e85bc8986820cce72e6549c6dfff0d3ee2b
Administrator <sljoai@outlook.com>
2022-04-21 20:12:00 +0800

    feat:logger

--- demo/src/main/java/com/txliu/demo/practice996/PersonTest.java
+++ demo/src/main/java/com/txliu/demo/practice996/PersonTest.java
@@ -1,12 +1,19 @@
 package com.txliu.demo.practice996;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @ClassName : PersonTest
  * @Author : txliu
  * @Date: 2022-04-20 21:16
  */
+@Slf4j
 public class PersonTest {
     public static void main(String[] args) {
         Person person = new Person();
+        log.info("nihao");
+        log.info("nihao");
+        log.info("nihao");
+        log.error("nihao");
     }
 }
