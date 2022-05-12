dc880b815359773e5225b848a1d5331b41218d69
fh_txliu <sljoai@outlook.com>
2022-05-12 19:37:02 +0800

    feat:person  assembly.xml

--- demo/src/main/java/com/txliu/demo/practice996/PersonTest.java
+++ demo/src/main/java/com/txliu/demo/practice996/PersonTest.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.txliu.demo.demo.PropertiesDemo;
 import org.apache.log4j.Logger;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,7 @@ import java.util.Map;
  */
 public class PersonTest {
     public static final Logger logger = Logger.getLogger(PersonTest.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Person person = new Person();
         System.out.println("123523625");
         Map<String, String> map = new HashMap<>();
@@ -24,5 +25,18 @@ public class PersonTest {
         logger.info("123456");
         PropertiesDemo.propertiesTest();
         System.out.println("conf");
+        InputStream resourceAsStream = PersonTest.class.getClassLoader().getResourceAsStream("test.txt");
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("conf/test.properties")));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceAsStream));
+        String line = null;
+        while (true) {
+            try {
+                if (!((line = bufferedReader.readLine()) != null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(line);
+        }
+        bufferedReader.close();
     }
 }
